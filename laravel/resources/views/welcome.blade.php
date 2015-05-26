a2329976d642fa80002a443e3cae10a31ddd0679
Jason Pharis <rollingrock16@gmail.com>
2015-05-26 18:19:54 -0500

    added splash

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -40,8 +40,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<img src="img/cvw3_splash.jpg" />
 			</div>
 		</div>
 	</body>
