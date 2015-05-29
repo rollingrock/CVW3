1b820841b89a842286400bc9ba4cadc8e328a724
Jason Pharis <rollingrock16@gmail.com>
2015-05-28 22:18:13 -0500

    fixed floating menu

--- laravel/resources/views/mock.blade.php
+++ laravel/resources/views/mock.blade.php
@@ -17,9 +17,7 @@
 </head>
 <body>
     <section id="splash_bg" class="splash_bg">
-
-
-        <nav class="navbar navbar-default">
+        <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -38,12 +36,30 @@
 
                     <ul class="nav navbar-nav navbar-right">
 
-                            <li><a href="#">Login</a></li>
-                            <li><a href="#">Register</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Register</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
+        <div id="header" class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="header">Virtual</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="header">Carrier</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p class="header">Wing <strong>3</strong></p>
+                </div>
+            </div>
+
+        </div>
     </section>
 
     <section id="welcome" class="welcome">
@@ -61,7 +77,6 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </div>
-        </div>
         </div>
     </section>
 
