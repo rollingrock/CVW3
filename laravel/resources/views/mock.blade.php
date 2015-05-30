5027adbfe7309bdc49327323af77a16a6f305f42
Jason Pharis <rollingrock16@gmail.com>
2015-05-30 15:51:54 -0500

    migrated to using sass to configure bootstrap variables instead of mockupcss

--- laravel/resources/views/mock.blade.php
+++ laravel/resources/views/mock.blade.php
@@ -26,7 +26,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">VirtualCarrierWing <strong>3</strong></a>
+                    <a class="navbar-brand make-border-right" href="#">VirtualCarrierWing <strong>3</strong></a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
