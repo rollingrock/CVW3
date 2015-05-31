1f3861f3c83e5a175695e54f9602d4718204ca4c
Jason Pharis <rollingrock16@gmail.com>
2015-05-31 11:15:52 -0500

    made navs partials....also finished some more styling for Nav

--- laravel/resources/views/mock.blade.php
+++ laravel/resources/views/mock.blade.php
@@ -17,48 +17,9 @@
 </head>
 <body>
     <section id="splash_bg" class="splash_bg">
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand make-border-right" href="#">VirtualCarrierWing <strong>3</strong></a>
-                </div>
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Squadrons <span class="caret"></span></a>
-                            <ul class="dropdown-menu header-bg" role="menu">
-                                <li><a href="#">VFA-32 Swordsmen</a></li>
-                                <li><a href="#">VFA-37 Ragin' Bulls</a></li>
-                                <li><a href="#">VMFA-312 CheckerBoards</a></li>
-                                <li><a href="#">VFA-105 Gunslingers</a></li>
-                                <li><a href="#">VAW-126 Seahawks</a></li>
-                                <li><a href="#">VAQ-130 Zappers</a></li>
-                                <li><a href="#">VRS-40 Det I Rawhides</a></li>
-                                <li><a href="#">HS-7 Dusty Dogs</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Ready Room</a></li>
-                        <li><a href="#">Forums</a></li>
-                        <li><a href="#">Training</a></li>
-                        <li><a href="#">Downloads</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
+        @include('partials.topnav')
 
-                    <ul class="nav navbar-nav navbar-right">
-
-                        <li><a href="#">Login</a></li>
-                        <li><a href="#">Apply</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
         <div id="header" class="container">
             <div class="row">
                 <div class="col-md-12 col-xs-12">
---
+++ laravel/resources/views/partials/topnav.blade.php
@@ -0,0 +1,63 @@
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand make-border-right" href="#">VirtualCarrierWing <strong>3</strong></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Squadrons <span class="caret"></span></a>
+                    <ul class="dropdown-menu header-bg" role="menu">
+                        <li><a href="#">VFA-32 Swordsmen</a></li>
+                        <li><a href="#">VFA-37 Ragin' Bulls</a></li>
+                        <li><a href="#">VMFA-312 CheckerBoards</a></li>
+                        <li><a href="#">VFA-105 Gunslingers</a></li>
+                        <li><a href="#">VAW-126 Seahawks</a></li>
+                        <li><a href="#">VAQ-130 Zappers</a></li>
+                        <li><a href="#">VRS-40 Det I Rawhides</a></li>
+                        <li><a href="#">HS-7 Dusty Dogs</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Ready Room</a></li>
+                <li><a href="#">Forums</a></li>
+                <li class="dropdown visible-sm">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">More <span class="caret"></span></a>
+                    <ul class="dropdown-menu header-bg" role="menu">
+                        <li><a href="#">Training</a></li>
+                        <li><a href="#">Downloads</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </li>
+                <li class="hidden-sm"><a href="#">Training</a></li>
+                <li class="hidden-sm"><a href="#">Downloads</a></li>
+                <li class="hidden-sm"><a href="#">Contact Us</a></li>
+                <li class="visible-xs"><a href="#">Log Out</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right hidden">
+
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Apply</a></li>
+            </ul>
+            <div class="btn-group hidden-xs pull-right">
+                <button type="button" class="btn btn-primary dropdown-toggle navbar-btn" data-toggle="dropdown" aria-expanded="false">
+                    <span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp RollingRock
+                    <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Profile</a></li>
+                    <li role="presentation" class="divider"></li>
+                    <li><a href="#">Log Out</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
