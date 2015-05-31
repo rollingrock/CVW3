9b5e8ca2778598e36ab0bba95026fbc0201bd0b8
Jason Pharis <rollingrock16@gmail.com>
2015-05-31 16:07:19 -0500

    Marked up MIssion planner plage for Flight planner

--- laravel/resources/views/mock_mission.blade.php
+++ laravel/resources/views/mock_mission.blade.php
@@ -17,7 +17,92 @@
 </head>
 <body>
 
-@include('partials.topnav');
+@include('partials.topnav')
+
+<div class="jumbotron banner">
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="airborne text-center"><strong><span class="glyphicon glyphicon-star"></span> Mission Planner <span class="glyphicon glyphicon-star"></span></strong></h1>
+        </div>
+    </div>
+
+    <div role="tabpanel">
+
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active">
+                <a href="#Planner" aria-controls="planner" role="tab" data-toggle="tab">Planner</a>
+            </li>
+            <li role="presentation">
+                <a href="#ActiveFlt" aria-controls="activeflt" role="tab" data-toggle="tab">Active Flight</a>
+            </li>
+            <li role="presentation">
+                <a href="#Logbook" aria-controls="logbook" role="tab" data-toggle="tab">Log Book</a>
+            </li>
+        </ul>
+
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="Planner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="airborne">Planned Flights</h2>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="table-responsive">
+                            <table class="table table-condensed table-striped text-center">
+                                <tr>
+                                    <th class="text-center">Flight Number</th>
+                                    <th class="text-center">Departure</th>
+                                    <th class="text-center">Arrival</th>
+                                    <th class="text-center">Callsign</th>
+                                    <th class="text-center">Scheduled Date</th>
+                                    <th class="text-center">Scheduled Time</th>
+                                    <th class="text-center">Type</th>
+                                    <th class="text-center">Route</th>
+                                    <th class="text-center">Status</th>
+                                    <th class="text-center">Delete</th>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>KNTU</td>
+                                    <td>KNTU</td>
+                                    <td>CANYON1</td>
+                                    <td>5/31/15</td>
+                                    <td>19:30</td>
+                                    <td>Training</td>
+                                    <td>KNTU YERIN5</td>
+                                    <td><span class="label label-info text-center">PLANNED</span></td>
+                                    <td class="text-center"><a href="#"><span class="glyphicon glyphicon-remove" style="color : red"></span></a></td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>KNTU</td>
+                                    <td>KRNO</td>
+                                    <td>CANYON1</td>
+                                    <td>5/30/15</td>
+                                    <td>19:30</td>
+                                    <td>Training</td>
+                                    <td>KNTU YERIN5 KRNO</td>
+                                    <td><span class="label label-success">ACTIVE</span></td>
+                                    <td><a href="#"><span class="glyphicon glyphicon-remove" style="color : red"></span></a></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <button class="btn btn-success">Plan New Flight</button>
+                    </div>
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane" id="ActiveFlt"></div>
+            <div role="tabpanel" class="tab-pane" id="Logbook"></div>
+        </div>
+
+    </div>
+
+</div>
 
 @include('partials.footer')
 
